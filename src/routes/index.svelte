ff1a8c9c44b1b1efe74afaf2d74cb17960f90149
dburandt <behringer.19@gmail.com>
2020-08-08 14:36:39 -0400

    Tweak homepaeg

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,15 +7,37 @@
     text-align: center;
     margin: 1em auto;
   }
+
+  div {
+    display: grid;
+    grid-template-columns: 500px;
+    justify-content: center;
+    margin-top: 22px;
+  }
+
+  a {
+    display: block;
+  }
+
+  img {
+    width: 22px;
+    margin-top: 8px;
+  }
 </style>
 
 <svelte:head>
-  <title>derek's site</title>
+  <title>Derek's Site</title>
 </svelte:head>
 
-<p>
-  Hello, my name is
-  <strong>Derek Burandt</strong>
-  and this is my personal webpage. Stay tuned for some future blog posts! Until
-  then, here's my github:
-</p>
+<div>
+  <p>
+    Hello, my name is
+    <strong>Derek Burandt</strong>
+    and this is my personal webpage. I'm a software developer based out of
+    Toronto Ontario, 🇨🇦. Stay tuned for some future blog posts! Until then,
+    here's my github:
+    <a href="https://github.com/dburandt">
+      <img src="github-icon.png" alt="github" />
+    </a>
+  </p>
+</div>
